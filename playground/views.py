6f31463c8b518f5573bcc4be355c1dbf860f06f1
terminal-guy <vishaladhithya19@gmail.com>
2021-07-01 19:20:30 +0530

    adding debug toolbar in django

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,13 @@
+from typing import Mapping
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def calculate():
+	x = 1
+	y = 2
+	return x
+
 def say_hello(request):
-	return HttpResponse('hello world')
+	x = calculate()
+	return render(request, 'hello.html',{'name': 'Mosh'})
